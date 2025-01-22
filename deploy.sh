bed4c5e0ff646c52a361da67797fdf45c610cbf4
Niko Köbler <niko@n-k.de>
2025-01-22 20:18:01 +0100

    manual deployment

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --rm -e TZ=Europe/Berlin -v .:/srv/jekyll:cached -v jekyll_data:/usr/local/bundle jekyll/jekyll:4 jekyll build
+export SSHPASS=$(aws ssm get-parameter --name strato-masterpwd --with-decryption | jq -r .Parameter.Value)
+echo "Upload files... please wait."
+sshpass -e scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r _site/* n-k.de@ssh.strato.de:/keycloak-dev-day/
+echo "Deployment done."
